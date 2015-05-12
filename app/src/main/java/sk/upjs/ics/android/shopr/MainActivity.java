8ae6c3c5c5a2be8f3d6f7b6db3d3a3ef5649fb3b
novotnyr <robert.novotny@upjs.sk>
2015-05-12 12:23:15 +0200

    Initial commit

---
+++ app/src/main/java/sk/upjs/ics/android/shopr/MainActivity.java
@@ -0,0 +1,67 @@
+package sk.upjs.ics.android.shopr;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+
+public class MainActivity extends ActionBarActivity implements AdapterView.OnItemClickListener {
+    private ActionBarDrawerToggle drawerToggle;
+    private DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+
+        drawerToggle = new ActionBarDrawerToggle(this,
+                drawerLayout,
+                R.string.openDrawer,
+                R.string.closeDrawer
+        );
+
+        drawerLayout.setDrawerListener(drawerToggle);
+
+        ListView navigationListView = (ListView) findViewById(R.id.navigationListView);
+        navigationListView.setOnItemClickListener(this);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        String selectedItemTitle = (String) parent.getAdapter().getItem(position);
+        setTitle(selectedItemTitle);
+
+        drawerLayout.closeDrawers();
+    }
+}
